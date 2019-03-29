dd71e90efdc337ea227ab07b7efff666d9723e32
Long H Nguyen <45839304+datasciencecpa@users.noreply.github.com>
2019-03-29 17:22:51 -0500

    Add files via upload

--- Soccer/app.R
+++ Soccer/app.R
@@ -2,14 +2,15 @@ library(shiny)
 library(shinyjs)
 library(DT)
 library(dplyr)
+# library(tidyverse)
 ui <- fluidPage(
   useShinyjs(),
   title = "FIFA Soccer 2019 App",
   sidebarLayout(
     sidebarPanel(
       radioButtons("selection", label = h3("Select one of the options below:"), 
-                   choiceNames = c("Select Player By Name", " Select By Club", "Advanced Search"),
-                   choiceValues = c("Name","Club", "Advanced"),
+                   choiceNames = c("Select Player By Name", " Select By Club", "Advanced Search", "Fun Facts"),
+                   choiceValues = c("Name","Club", "Advanced", "Fun"),
                    inline = FALSE),
       hr(),
       conditionalPanel(
@@ -26,24 +27,36 @@ ui <- fluidPage(
       conditionalPanel(
         condition = "input.selection == 'Advanced'",
         h3("Advanced Search"),
-        selectInput("positions",label = "Available Positions", choices = NULL, selectize = FALSE),
+        selectInput("positions",label = "Available Positions", choices = NULL, multiple = TRUE),
         sliderInput("Ages", label = "Player Ages", min=15, max=55, value = c(15,50))
-      ) # Third conditional for search player Advanced Search
+      ),# Third conditional for search player Advanced Search
+      conditionalPanel(
+        condition = "input.selection == 'Fun'",
+        radioButtons("funSelection", label = h3("Choose one of the options belows:"),
+                     choiceNames = c("View Club Stats", "Top 10 Most Valuable Players", "Best 10 By Positions"), 
+                     choiceValues = c("clubStats", "valuable10", "position10"), inline = FALSE)
+      ),
+      conditionalPanel(
+        condition = "input.funSelection == 'position10'",
+        selectInput("positions2", label = "Available Positions", choices = NULL),
+        sliderInput("Ages2", label = "Player Ages", min=15, max=55, value = c(15,50))
+      )
     ), # End sidebarPanel
     mainPanel(
       tags$img(src = "soccerball.jpeg",height = "100", width = "100"),
       dataTableOutput("nameTbl"),
-      dataTableOutput("clubTbl"),
-      dataTableOutput("otherTbl")
+      dataTableOutput("FunTbl")
     )
   ) # End sidebarLayout
 )
 
 server <- function (input, output, session){
-  fifaData <- read.csv("data.csv", stringsAsFactors = FALSE)
-  fifaData <- na.omit(fifaData)
+
+  fifaData <- read.csv("data.csv", na.strings = c(""), stringsAsFactors = FALSE )
   fifaData$Image <- paste("<img src='",fifaData$Photo,"'></img>", sep="")
-  fifaData$ClubImage <- paste("<img src='", fifaData$Club.Logo,"'></img>", sep ="")
+  fifaData$ClubImage <- paste("<img src='", fifaData$ClubLogo,"'></img>", sep ="")
+  fifaData$FlagImg <- paste("<img src='", fifaData$Flag,"'></img>", sep ="")
+  
   formatValue <- function (value){
     n = nchar(value)
     measure <- substr(value, n,n)
@@ -51,64 +64,92 @@ server <- function (input, output, session){
     amt <- substr(value, 2, n-1)
 
     amt <- as.numeric(amt)
-    if (measure =="K") {
-      amt <- amt *1000
-    } else {
-      amt <- amt*1000000
-    } 
+    if (n>=3){
+      if (measure =="K") {
+        amt <- amt *1000
+      } else {
+        amt <- amt*1000000
+      } 
+    }
     return (amt)
   }
-  fifaData$Value <- lapply(fifaData$Value, formatValue)
+  fifaData$Value <- as.numeric(lapply(fifaData$Value, formatValue))
+  fifaData$Wage <- as.numeric(lapply(fifaData$Wage, formatValue))
+
   positions <- unique(sort(fifaData$Position))
   ageRange <- range(fifaData$Age)
-  
-  # print (str(fifaData))
-  observe({
-    if (input$selection =="Name") {
-      hide("clubTbl")
-      hide("otherTbl")
-      show("nameTbl")
-    }
-    if (input$selection == "Club"){
-      show("clubTbl")
-      hide("otherTbl")
-      hide("nameTbl")
-    }
-    if (input$selection == "Advanced"){
-      updateSelectInput(session,"positions",choices = positions)
-      updateSliderInput(session, "Ages", min= ageRange[1], max= ageRange[2])
-      hide("clubTbl")
-      show("otherTbl")
-      hide("nameTbl")
 
+  observe({
+    if (input$selection == "Advanced"){
+      updateSelectInput(session,"positions",choices = positions, selected = positions[1])
+      updateSliderInput(session, "Ages", min= ageRange[1], max= ageRange[2])
     }
+    if (input$selection =="Fun"){
+      shinyjs::hide ("nameTbl")
+      shinyjs::show ("FunTbl")
+    } else if (input$selection != "Fun") {
+      updateRadioButtons(session,"funSelection", selected = "clubStats") # so that select input position will be hidden.
+      shinyjs::hide ("FunTbl")
+      shinyjs::show ( "nameTbl")
+    }
+    if (input$funSelection == "position10"){
+      updateSelectInput(session,"positions2",choices = positions, selected = positions[1])
+      updateSliderInput(session, "Ages2", min= ageRange[1], max= ageRange[2])
+    }
+  })
+  dataOutput <- reactive ({
+    db <- data.frame()
+    if (input$selection =="Name"){
+      if (input$searchName !=""){
+        index = grepl(input$searchName, fifaData$Name, ignore.case = TRUE)
+        db <- fifaData[index,]%>% select(Name, Image, Age, Nationality, FlagImg, Club, ClubImage,Position, Value)
+        
+      } else {
+        db <- fifaData[1:10,] %>% select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Value)
+      }
+    } # End input$selection == Name
+    else if (input$selection =="Club"){
+      if (input$searchClub !=""){
+        index = grepl(input$searchClub, fifaData$Club, ignore.case = TRUE)
+        db <- fifaData[index,]%>% select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Value)
+      } else {
+        db <- fifaData[1:10,] %>% select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Value)
+      }
+    } # End input$selection == "Club"
+    else {
+      db <- fifaData %>% filter(Position == input$positions & between (Age, input$Ages[1], input$Ages[2])) %>% 
+        select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Value)
+    }
+    return (datatable(db,options = list(pageLength = 25), escape= FALSE) %>% formatCurrency(9, currency = "€"))
+  })
+  funDataOutput <- reactive({
+    db <- data.frame()
+    currencyCol <- c(0)
+    if (input$funSelection =="clubStats"){
+      # Group by Club
+      db <- fifaData %>% group_by(Club) %>% 
+          summarize(Average_Age = round(mean(Age)), Youngest_Player_Age = min(Age), Oldest_Player_Age = max(Age),
+                    Average_Wages = mean(Wage),Total_Wages_Paid = sum(Wage),Total_Player_Value = sum(Value))
+      currencyCol <- c(5:7)
+    } else if (input$funSelection == "valuable10") {
+      db <- fifaData %>% arrange(desc(Value)) # First sorted the table by descending value
+      db <- db[1:10,] #getting only 10 top rows
+      db <- db %>% select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Value)
+      currencyCol <- 9
+    } else if (input$funSelection == "position10"){
+      db <- fifaData %>%filter(Position == input$positions2 & between (Age, input$Ages2[1], input$Ages2[2]))
+      db <- db %>%arrange(desc(Overall))
+      db <- db[1:10,]
+      db <- db %>% select(Name, Image, Age, Nationality,FlagImg, Club, ClubImage,Position, Overall, Value)
+      currencyCol <- 10
+    }
+    return (datatable(db,options = list(pageLength = 25), escape= FALSE) %>% formatCurrency(currencyCol, currency = "€"))
   })
   output$nameTbl <- renderDataTable({
-    db <- data.frame()
-    if (input$searchName !=""){
-      index = grepl(input$searchName, fifaData$Name, ignore.case = TRUE)
-      db <- fifaData[index,]%>% select(Name, Image, Age, Nationality, Club, ClubImage,Position, Value)
-
-    } else {
-      db <- fifaData[1:10,] %>% select(Name, Image, Age, Nationality, Club, ClubImage,Position, Value)
-    }
-    return (datatable(db,options = list(pageLength = 25), escape= FALSE) %>% formatCurrency(8, currency = "€"))
+    dataOutput()
   })
-  output$clubTbl <- renderDataTable({
-    db <- data.frame()
-    if (input$searchClub !=""){
-      index = grepl(input$searchClub, fifaData$Club, ignore.case = TRUE)
-      db <- fifaData[index,]%>% select(Name, Image, Age, Nationality, Club, ClubImage,Position, Value)
-    } else {
-      db <- fifaData[1:10,] %>% select(Name, Image, Age, Nationality, Club, ClubImage,Position, Value)
-    }
-    return (datatable(db, options = list(pageLength = 25),escape= FALSE) %>% formatCurrency(8, currency = "€"))
-  })
-  output$otherTbl <- renderDataTable({
-    db <- data.frame()
-    db <- fifaData %>% filter(Position == input$positions & between (Age, input$Ages[1], input$Ages[2])) %>% 
-          select(Name, Image, Age, Nationality, Club, ClubImage,Position, Value)
-    return (datatable(db, options = list(pageLength = 25),escape= FALSE) %>% formatCurrency(8, currency = "€"))
+  output$FunTbl <- renderDataTable({
+    funDataOutput()
   })
 }
 shinyApp(ui, server)
